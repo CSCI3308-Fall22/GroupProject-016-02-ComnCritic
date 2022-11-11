f757fc3df89525b524fec5d7cb645e55b4bb77fb
Musaad <musaads@MacBook-Pro-10.local>
2022-11-10 17:34:11 -0700

    Movies Data 0.1v

--- Project Code/init_data/create.sql
+++ Project Code/init_data/create.sql
@@ -3,3 +3,9 @@ CREATE TABLE users(
     username VARCHAR(50) PRIMARY KEY,
     password CHAR(60) NOT NULL
 );
+
+CREATE TABLE movies(
+    
+    -- there should be a movie_name, skos_count, image.png data types here, can someone implement it?
+    
+);
---
+++ Project Code/init_data/movies_data.sql
@@ -0,0 +1,4 @@
+insert into movies(movie_name, skos_count, img) values ('the good the bad the ugly', 3678, /img.png )
+
+
+-- this is just an example, can someone conform if this is how should i do it?
