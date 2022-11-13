789b6b68a081cbaab31e5155ab3bca9d6cab76d1
James Hautekeete <jaha5099@colorado.edu>
2022-11-12 18:17:22 -0700

    completed create

--- Project Code/init_data/create.sql
+++ Project Code/init_data/create.sql
@@ -1,5 +1,30 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
-    username VARCHAR(50) PRIMARY KEY,
+    users_id SERIAL PRIMARY KEY,
+    email VARCHAR(50) NOT NULL,
     password CHAR(60) NOT NULL
+);
+
+DROP TABLE IF EXISTS movies CASCADE;
+CREATE TABLE movies(
+    movie_id SERIAL PRIMARY KEY,
+    movie_name VARCHAR(50) NOT NULL,
+    movie_image VARCHAR(80) NOT NULL,
+    duration SMALLINT NOT NULL,
+    describe VARCHAR(100) NOT NULL,
+    skoNo BIGINT NOT NULL
+);
+
+DROP TABLE IF EXISTS reviews CASCADE;
+CREATE TABLE reviews(
+    review_id SERIAL PRIMARY KEY,
+   
+    PRIMARY KEY(contact_id),
+    CONSTRAINT fk_user
+        FOREIGN KEY(users_id) 
+	        REFERENCES users(users_id),
+    CONSTRAINT fk_movie
+        FOREIGN KEY(movie_id)
+            REFERENCES movies(movie_id),
+    review_text VARCHAR(100) NOT NULL,
 );
